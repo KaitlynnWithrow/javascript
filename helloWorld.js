01ec0521bd12ecc03e659d79fc87c24af731d78e
kaitlynn <kaitlynnwithrow@gmail.com>
2021-12-30 21:32:02 -0500

    comments on basic js

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+console.log("hello World")
+// a way to print somthing inside the console
+var array = [5, 9, 6, 3 ]
+// an array is a list inside of square brackets,can take any data type
+var string = "5 9 6 3" // must be in quotes to be a string
+var int = 5 // if in quotes it is a string
+floating numbers
+// floating numbers contain floating decimal points such as 5.5
+condithional
+//"condithionals are statments written by the programer which evaluat the actions"
+funtions
+//"self contand code that accomlish a specific task"
+obgects
+// "an obgect is a standalone entity,with propertis and type"
